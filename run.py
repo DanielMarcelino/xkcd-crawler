8a8419060ac27f6c6ba294aaef53b79326d37b81
Daniel Marcelino Azevedo <daniel.azevedo@justicafacil.com.br>
2022-01-30 18:05:39 -0300

    Cria run.py

---
+++ run.py
@@ -0,0 +1,16 @@
+from xkcd_downloader import XkcdDownloader
+
+
+def main():
+    xkcd_downloader_instance = XkcdDownloader()
+    xkcd_downloader_instance.make_download()
+    print('End of execution')
+    print(
+        (f'Resume: {xkcd_downloader_instance.get_count_of_comic_downloads}')
+        (' comics image files has been downloaded and saved ')
+        (f'in {xkcd_downloader_instance.DIRECTORY}/')
+    )
+
+
+if __name__ == '__main__':
+    main()
